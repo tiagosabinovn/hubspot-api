efe333b509dbfdcbf6111291215b951fd4465efd
tiagosabinovn <tiagosabinovn@gmail.com>
2024-08-09 11:32:27 -0300

    Delete Oauth2 directory

--- Oauth2/desactive_localhost.bash
+++
@@ -1,2 +0,0 @@
-sudo netstat -tuln | grep 3000
-sudo ufw allow 3000/tcp
--- Oauth2/test_command.bash
+++
@@ -1,4 +0,0 @@
-  curl -X GET \
-  'https://api.hubapi.com/crm/v3/companies' \
-  -H 'Authorization: Bearer seu_token_oauth_aqui' \
-  -H 'Content-Type: application/json'
